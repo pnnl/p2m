7c6635e8d4a643e720ad8dc0231026f2d69c89c9
christinehc <christine.chang@pnnl.gov>
2023-01-20 16:43:20 -0800

    chore: update repo URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
       description='Identify metabolites associated with a list of protein identifiers',
       author='Bryan J. Killinger',
       author_email='brykpnl@gmail.com',
-      url='https://github.com/brykpnl/p2m',
+      url='https://github.com/pnnl/p2m',
       packages=find_packages(),
       install_requires=['libchebipy'],
       entry_points={
