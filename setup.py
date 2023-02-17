e7bd43144faa75c926d8a40d4e4c71fa49f95681
christinehc <christine.chang@pnnl.gov>
2023-02-17 11:49:39 -0800

    build: require sparqlwrapper
    changelog:
    - sparqlwrapper is now required for SPARQL-based code
    - NOTE: libchebipy has not yet been removed as a requirement but is no longer required for the main workflow

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required = [x for x in f.read().splitlines() if not x.startswith("#")]
+
 setup(
-      name='p2m',
-      version='1.0.0',
-      description='Identify metabolites associated with a list of protein identifiers',
-      author='Bryan J. Killinger',
-      author_email='brykpnl@gmail.com',
-      url='https://github.com/pnnl/p2m',
-      packages=find_packages(),
-      install_requires=['libchebipy'],
-      entry_points={
-          'console_scripts': ['p2m=p2m.main:main']
-          },
-      )
-      
-      
+    name="p2m",
+    version="1.0.0",
+    description="Identify metabolites associated with a list of protein identifiers",
+    author="Pacific Northwest National Laboratory",
+    author_email="brykpnl@gmail.com",
+    url="https://github.com/pnnl/p2m",
+    packages=find_packages(exclude=("test")),
+    install_requires=required,
+    entry_points={"console_scripts": ["p2m=p2m.main:main"]},
+)
